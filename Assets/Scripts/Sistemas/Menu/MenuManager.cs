5e55607b3f516228ef140407f792c33912c969d0
Ojogotcc <ojogotcc24@gmail.com>
2024-09-13 12:18:29 -0300

    Atualizacoes da alpha

--- Assets/Scripts/Sistemas/Menu/MenuManager.cs
+++ Assets/Scripts/Sistemas/Menu/MenuManager.cs
@@ -5,57 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    [SerializeField] public string fase;
+    [SerializeField] public string Fase1;
 
-    public GameObject menuPrincipal;
-    public GameObject menuOpcoes;
-    public GameObject menuCreditos;
-    public GameObject menuPesquisa;
+    // public GameObject menuPrincipal;
+    // public GameObject menuOpcoes;
+    // public GameObject menuCreditos;
+    // public GameObject menuPesquisa;
     
     public void AbrirJogo()
     {
-        SceneManager.LoadScene(fase);
+        SceneManager.LoadScene(Fase1);
     }
 
     public void FecharJogo()
     {
-        Debug.Log("Sair do jogo");
         Application.Quit();
     }
 
-    public void AbrirOpcoes()
-    {
-        menuPrincipal.SetActive(false);
-        menuOpcoes.SetActive(true);
-    }
+    // public void AbrirOpcoes()
+    // {
+    //     menuPrincipal.SetActive(false);
+    //     menuOpcoes.SetActive(true);
+    // }
 
-    public void FecharOpcoes()
-    {
-        menuOpcoes.SetActive(false);
-        menuPrincipal.SetActive(true);
-    }
+    // public void FecharOpcoes()
+    // {
+    //     menuOpcoes.SetActive(false);
+    //     menuPrincipal.SetActive(true);
+    // }
 
-    public void AbrirCreditos()
-    {
-        menuPrincipal.SetActive(false);
-        menuCreditos.SetActive(true);
-    }
+    // public void AbrirCreditos()
+    // {
+    //     menuPrincipal.SetActive(false);
+    //     menuCreditos.SetActive(true);
+    // }
 
-    public void FecharCreditos()
-    {
-        menuCreditos.SetActive(false);
-        menuPrincipal.SetActive(true);
-    }
+    // public void FecharCreditos()
+    // {
+    //     menuCreditos.SetActive(false);
+    //     menuPrincipal.SetActive(true);
+    // }
 
-    public void AbrirPesquisa()
-    {
-        menuPrincipal.SetActive(false);
-        menuPesquisa.SetActive(true);
-    }
+    // public void AbrirPesquisa()
+    // {
+    //     menuPrincipal.SetActive(false);
+    //     menuPesquisa.SetActive(true);
+    // }
 
-    public void FecharPesquisa()
-    {
-        menuPesquisa.SetActive(false);
-        menuPrincipal.SetActive(true);
-    }
+    // public void FecharPesquisa()
+    // {
+    //     menuPesquisa.SetActive(false);
+    //     menuPrincipal.SetActive(true);
+    // }
 }
